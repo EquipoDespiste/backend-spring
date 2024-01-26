63c23ec43f471a95f3aed671fea9280edee5902f
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-01-26 13:19:36 +0100

    Terminado Issue#04 : Add @Repository

--- ProyectoMoviles/src/main/java/com/adorno/repositorio/IMovilRepositorio.java
+++ ProyectoMoviles/src/main/java/com/adorno/repositorio/IMovilRepositorio.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.adorno.modelo.Movil;
 
+@Repository
 public interface IMovilRepositorio extends JpaRepository<Movil, Long>{
 
 

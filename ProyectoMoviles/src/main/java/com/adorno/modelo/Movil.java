2224d77b279acd0e2bf93434fb099edd03c8f22c
Inma <iperezs24m@gmail.com>
2024-01-27 11:49:08 +0100

    Test movil verde
    test movil con mocks y verde

--- ProyectoMoviles/src/main/java/com/adorno/modelo/Movil.java
+++ ProyectoMoviles/src/main/java/com/adorno/modelo/Movil.java
@@ -17,6 +17,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Movil {
+<<<<<<< Updated upstream
+=======
+	
+	
+	public Movil(String modelo, String marca, int almacenamiento_gb, int ram, float peso, int camara, int bateria,
+			boolean nfc, int contador_visita, float precio_actual) {
+		super();
+		this.modelo = modelo;
+		this.marca = marca;
+		this.almacenamiento_gb = almacenamiento_gb;
+		this.ram = ram;
+		this.peso = peso;
+		this.camara = camara;
+		this.bateria = bateria;
+		this.nfc = nfc;
+		this.contador_visita = contador_visita;
+		this.precio_actual = precio_actual;
+	}
+>>>>>>> Stashed changes
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- ProyectoMoviles/src/test/java/com/adorno/services/MovilServiceTest.java
+++ ProyectoMoviles/src/test/java/com/adorno/services/MovilServiceTest.java
@@ -2,18 +2,27 @@ package com.adorno.services;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.adorno.modelo.Movil;
+<<<<<<< Updated upstream
 import com.adorno.repositorio.IMovilRepositorio;
 
 import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
 
 
+=======
+>>>>>>> Stashed changes
 
 class MovilServiceTest {
 
+<<<<<<< Updated upstream
 @Autowired
 MovilService movilservicio= new MovilService();
 @Autowired
@@ -49,5 +58,57 @@ IMovilRepositorio movilrepo;
 		
 	}
 	
+=======
+	@Autowired
+	MovilService movilservicio = new MovilService();
+
+	private List<Movil> listaMoviles = new ArrayList<>();
+
+	private Movil movil;
+
+	@BeforeEach
+	void before() {
+		listaMoviles.add(new Movil("modelo1", "Marca1", 1, 2, 1f, 2, 1, false, 1, 1f));
+		movil = new Movil("modelo1", "Marca1", 1, 2, 1f, 2, 1, false, 1, 1f);
+	}
+
+	@Test
+	void testAdd() {
+		 movil = Mockito.mock(Movil.class);
+		assertTrue(movilservicio.add(movil));
+	}
+
+	@Test
+	void testDelete() {
+
+		movilservicio.add(movil);
+		assertTrue(movilservicio.delete(1L));
+	}
+
+	@Test
+	void testGetMovilById() {
+
+		movilservicio.add(movil);
+		assertTrue(movilservicio.getById(1L).isPresent());
+
+	}
+
+	@Test
+	void testFindAll() {
+		 movil = Mockito.mock(Movil.class);
+		movilservicio.add(movil);
+		assertTrue(!movilservicio.findAll().isEmpty());
+	}
+
+	@Test
+	void testInsertarLista() {
+		movilservicio.addAll(listaMoviles);
+		listaMoviles.forEach((movil) -> {
+			System.out.println(movil.getMarca());
+		});
+		System.out.println(movilservicio.findAll().isEmpty());
+		assertTrue(!movilservicio.findAll().isEmpty());
+	}
+>>>>>>> Stashed changes
 
 }

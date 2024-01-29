d9e191d03a9bf19c5131c6b252f10cdfaad0c357
rangeljuanma <jmrangelg04@iesarroyoharnina.es>
2024-01-29 10:30:16 +0100

    Terminado issue#22: solucion error findByMarca en movilServiceTest

--- ProyectoMoviles/src/test/java/com/adorno/services/MovilServiceTest.java
+++ ProyectoMoviles/src/test/java/com/adorno/services/MovilServiceTest.java
@@ -75,13 +75,13 @@ MovilService movilservicio= new MovilService();
 
 @Test
 void testFindBydMarca() {
-	movil = new Movil("modelo1", "Marca2", 1, 2, 1f, 2, 1, false, 1, 1f);
+	movil = new Movil("modelo1", 1, 2, 1f, 2, 1, false, 1, 1f);
 	listaMoviles.add(movil);
 	movilservicio.addAll(listaMoviles);
 	
 	int esperado=1;
-	int actual=movilservicio.findByMarca("Marca2").size();
-	assertTrue(movilservicio.findByMarca("Marca2")!=null);
+	int actual=movilservicio.findByMarca(1).size();
+	assertTrue(movilservicio.findByMarca(1)!=null);
 	assertEquals(esperado,actual);
 }
 }

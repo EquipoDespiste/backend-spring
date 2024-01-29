674115edf42e9d45f66a2dda1a80b6c28304253b
elenazambrano <95417079+elenazambrano@users.noreply.github.com>
2024-01-29 20:26:35 +0100

    terminado test marcaService

--- ProyectoMoviles/src/test/java/com/adorno/services/MarcaServiceTest.java
+++ ProyectoMoviles/src/test/java/com/adorno/services/MarcaServiceTest.java
@@ -1,24 +1,46 @@
 package com.adorno.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.adorno.modelo.Marca;
+
+@SpringBootTest
 class MarcaServiceTest {
 
+	@Autowired
+	MarcaService marcaService;
+	private List<Marca> listaMarcas;
+	private Marca marca;
+	
+	@BeforeEach
+	void before() {
+		listaMarcas = new ArrayList<>();
+	}
+	
 	@Test
 	void testAdd() {
-		fail("Not yet implemented");
+		assertTrue(marcaService.add("marca1"));
 	}
 
 	@Test
 	void testGetById() {
-		fail("Not yet implemented");
+		marcaService.add("marca1");
+		assertTrue(marcaService.getById(1L).isPresent());
 	}
 
 	@Test
 	void testFindAll() {
-		fail("Not yet implemented");
+		marcaService.add("marca1");
+		assertTrue(!marcaService.findAll().isEmpty());
 	}
 
 }

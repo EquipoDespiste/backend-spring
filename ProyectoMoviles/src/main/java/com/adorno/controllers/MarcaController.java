da4c72487445aca429e3b8260211d38d3bad56f9
elenazambrano <95417079+elenazambrano@users.noreply.github.com>
2024-01-30 10:20:32 +0100

    terminado controller de marca issue#16

--- ProyectoMoviles/src/main/java/com/adorno/controllers/MarcaController.java
+++ ProyectoMoviles/src/main/java/com/adorno/controllers/MarcaController.java
@@ -3,43 +3,47 @@ package com.adorno.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adorno.modelo.Marca;
+import com.adorno.services.MarcaService;
 
 @RestController
 @RequestMapping("marca")
-public class MarcaController implements Controller<Marca> {
+public class MarcaController implements Controller<String> {
 
+	private final MarcaService marcaService;
+	
+	public MarcaController(MarcaService marcaService) {
+		super();
+		this.marcaService = marcaService;
+	}
 	@Override
-	public List<Marca> all() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<String> all() {
+		return marcaService.findAll();
 	}
 
 	@Override
-	public Marca findById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public String findById(@PathVariable long id) {
+		return marcaService.getById(id).orElse("no existe la marca");
 	}
 
 	@Override
-	public boolean insert(Marca t) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean insert(@RequestBody String t) {
+		return marcaService.add(t);
 	}
 
 	@Override
-	public boolean insertAll(List<Marca> t) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean insertAll(@RequestBody List<String> t) {
+		return marcaService.addAll(t);
 	}
 
 	@Override
-	public boolean delete(long id) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean delete(@PathVariable long id) {
+		return marcaService.delete(id);
 	}
 
 }

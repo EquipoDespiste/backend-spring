8e0cc5442d205241f9eb103f580927655b2b9831
elenazambrano <95417079+elenazambrano@users.noreply.github.com>
2024-01-30 20:27:37 +0100

    terminado issue#45

--- ProyectoMoviles/src/main/java/com/adorno/modelo/Procesador.java
+++ ProyectoMoviles/src/main/java/com/adorno/modelo/Procesador.java
@@ -4,6 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,9 +27,22 @@ public class Procesador {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Min(value = 1)
+	@NotNull
 	private Long id;
+	@Size(min = 10, max = 75)
+	@NotBlank
+	@NotNull
+	@Pattern(regexp = "[a-zA-Z0-9]", message = "Este campo solo debe contener letras y/o numeros")
 	private String tipo;
+	@NotEmpty
+	@Min(value = 1)
+	@Max(value = 32)
+	@Positive
 	private int nucleos;
+	@NotEmpty
+	@Positive
+	@DecimalMax(value = "4.9", inclusive = true)
 	private float velocidad_max;
 	
 	public Procesador(String tipo, int nucleos, float velocidad_max) {

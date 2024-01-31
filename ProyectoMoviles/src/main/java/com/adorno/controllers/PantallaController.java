2a9e291f3c51972f098cc8459b8df17c66369590
IgnacioOI <94613709+IgnacioOI@users.noreply.github.com>
2024-01-31 16:27:25 +0100

    #issue63
    Añadir al controller los Response

--- ProyectoMoviles/src/main/java/com/adorno/controllers/PantallaController.java
+++ ProyectoMoviles/src/main/java/com/adorno/controllers/PantallaController.java
@@ -1,12 +1,16 @@
 package com.adorno.controllers;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adorno.modelo.Movil;
 import com.adorno.modelo.Pantalla;
 import com.adorno.services.PantallaService;
 
@@ -15,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("pantallas")
-public class PantallaController implements Controller<Pantalla>{
+public class PantallaController {
 
 	private final PantallaService pantallaService;
 
@@ -24,25 +28,27 @@ public class PantallaController implements Controller<Pantalla>{
 		this.pantallaService = pantallaService;
 	}
 	
-	@Override
-	public List<Pantalla> all() {
-		return pantallaService.findAll();
+	@GetMapping("/all")
+	public ResponseEntity<List<Pantalla>> all() {
+		return Optional.of(new ResponseEntity<List<Pantalla>>(pantallaService.findAll(),HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
+
 	}
-	@Override
-	public Pantalla findById(@PathVariable long id) {
-		return pantallaService.getById(id).orElse(new Pantalla());
+	@GetMapping("/find/{id}")
+	public ResponseEntity<Pantalla> findById(@PathVariable long id) {
+		return Optional.of(new ResponseEntity<Pantalla>(pantallaService.getById(id).get(), HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
 	}
-	@Override
-	public boolean insert(@RequestBody Pantalla pantalla) {
-		return pantallaService.add(pantalla);
+	@PostMapping("/insercion")
+	public  ResponseEntity<Boolean>  insert(@RequestBody Pantalla pantalla) {
+		return Optional.of(new ResponseEntity<Boolean>(pantallaService.add(pantalla),HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
 	}
-	@Override
-	public boolean insertAll (@RequestBody List<Pantalla> pantalla) {
-		return pantallaService.addAll(pantalla);
+	@PostMapping("/insertar-lista")
+	public ResponseEntity<Boolean> insertAll (@RequestBody List<Pantalla> pantalla) {
+		return Optional.of(new ResponseEntity<Boolean>(pantallaService.addAll(pantalla),HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
 	}
-	@Override
-	public boolean delete(@PathVariable long id) {
-		return pantallaService.delete(id);
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> delete(@PathVariable long id) {
+		return Optional.of(new ResponseEntity<Boolean>(pantallaService.delete(id),HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
 	}
 
 }

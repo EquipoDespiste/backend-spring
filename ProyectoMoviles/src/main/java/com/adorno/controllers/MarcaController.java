656d7f4ce0fb055cd0928d04dd14eda17ee43235
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-02-01 00:52:56 +0100

    Done issue#65: implement ResponseEntity in MarcaController

--- ProyectoMoviles/src/main/java/com/adorno/controllers/MarcaController.java
+++ ProyectoMoviles/src/main/java/com/adorno/controllers/MarcaController.java
@@ -2,8 +2,15 @@ package com.adorno.controllers;
 
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +20,7 @@ import com.adorno.services.MarcaService;
 
 @RestController
 @RequestMapping("marca")
-public class MarcaController implements Controller<String> {
+public class MarcaController  {
 
 	private final MarcaService marcaService;
 	
@@ -21,29 +28,56 @@ public class MarcaController implements Controller<String> {
 		super();
 		this.marcaService = marcaService;
 	}
-	@Override
-	public List<String> all() {
-		return marcaService.findAll();
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<String>> all() {
+		List<String> marcas =  marcaService.findAll();
+		
+		return ResponseEntity.ok().body(marcas); 
 	}
 
-	@Override
-	public String findById(@PathVariable long id) {
-		return marcaService.getById(id).orElse("no existe la marca");
+	@GetMapping("/find/{id}")
+	public ResponseEntity<String> findById(@PathVariable long id) {
+		
+		Optional<String> marca = marcaService.getById(id);
+		
+		if(marca.isPresent())
+			return ResponseEntity.ok().body(marca.get());
+		
+		return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
 	}
 
-	@Override
-	public boolean insert(@RequestBody String t) {
-		return marcaService.add(t);
+	@PostMapping("/insercion")
+	public ResponseEntity<Boolean> insert(@RequestBody String t) {
+		
+		boolean isInsert= false;
+		
+		if(isInsert = marcaService.add(t)) {
+			return ResponseEntity.ok().body(isInsert);
+		};
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(isInsert);
 	}
 
-	@Override
-	public boolean insertAll(@RequestBody List<String> t) {
-		return marcaService.addAll(t);
+	@PostMapping("/insercion-lista")
+	public ResponseEntity<Boolean> insertAll(@RequestBody List<String> t) {
+		boolean isInsertAll= false;
+		
+		if(isInsertAll = marcaService.addAll(t)) {
+			return ResponseEntity.ok().body(isInsertAll);
+		};
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(isInsertAll);
 	}
 
-	@Override
-	public boolean delete(@PathVariable long id) {
-		return marcaService.delete(id);
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> delete(@PathVariable long id) {
+		boolean idDelete= false;
+		if(idDelete = marcaService.delete(id)) {
+			return ResponseEntity.ok().body(idDelete);
+		};
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(idDelete);
 	}
 
 }

38a52a15c8a8ef6d0e304c9e16134888f9fc71d3
IgnacioOI <94613709+IgnacioOI@users.noreply.github.com>
2024-02-02 12:33:06 +0100

    issue81
    movil populate arreglos

--- ProyectoMoviles/src/main/java/com/adorno/populate/MovilPopulate.java
+++ ProyectoMoviles/src/main/java/com/adorno/populate/MovilPopulate.java
@@ -28,20 +28,29 @@ private final MovilService movilService;
 		  moviles.add(new Movil("Galaxy S21", 128, 12, 160.0f, 16, 4500, true, 120, 799.99f,
 	                new Marca("Samsung"), new Pantalla(6.2f, "Dynamic AMOLED"),
 	                new Dimension(151.7f, 71.2f, 7.9f), new Procesador("Exynos 2100", 8, 2.9f)));
+		  moviles.add(new Movil("Galaxy S27", 128, 12, 160.0f, 16, 4500, true, 120, 299.99f,
+				  new Marca("Samsung"), new Pantalla(6.2f, "Dynamic AMOLED"),
+				  new Dimension(151.7f, 71.2f, 7.9f), new Procesador("Exynos 2100", 8, 2.9f)));
+		  moviles.add(new Movil("Galaxy S57", 128, 12, 160.0f, 16, 4500, false, 120, 299.99f,
+				  new Marca("Samsung"), new Pantalla(6.2f, "Dynamic AMOLED"),
+				  new Dimension(151.7f, 71.2f, 7.9f), new Procesador("Exynos 2100", 8, 2.9f)));
+		  moviles.add(new Movil("Galaxy S17", 128, 12, 160.0f, 16, 4500, false, 120, 299.99f,
+				  new Marca("Samsung"), new Pantalla(6.1f, "OLED"),
+				  new Dimension(151.7f, 71.2f, 7.9f), new Procesador("Exynos 2100", 8, 2.9f)));
 
 	        moviles.add(new Movil("iPhone 13", 256, 16, 173.0f, 12, 3687, true, 90, 999.99f,
 	                new Marca("Apple"), new Pantalla(6.1f, "Super Retina XDR"),
 	                new Dimension(146.7f, 71.5f, 7.7f), new Procesador("A15 Bionic", 6, 3.23f)));
 
 	        moviles.add(new Movil("OnePlus 9", 256, 12, 183.0f, 48, 4500, true, 110, 899.99f,
-	                new Marca("OnePlus"), new Pantalla(6.55f, "Fluid AMOLED"),
+	                new Marca("Samsung"), new Pantalla(6.55f, "Fluid AMOLED"),
 	                new Dimension(160.0f, 74.2f, 8.7f), new Procesador("Snapdragon 888", 8, 2.84f)));
 
-	        moviles.add(new Movil("Pixel 6", 128, 8, 183.8f, 50, 4600, true, 105, 899.99f,
-	                new Marca("Google"), new Pantalla(6.4f, "OLED"),
+	        moviles.add(new Movil("Redmi 6", 128, 8, 183.8f, 50, 4600, true, 105, 899.99f,
+	                new Marca("Xiaomi"), new Pantalla(6.4f, "OLED"),
 	                new Dimension(158.6f, 74.8f, 8.9f), new Procesador("Tensor", 8, 2.8f)));
 	        moviles.add(new Movil("Xperia 5 III", 128, 8, 168.0f, 12, 4500, true, 95, 899.99f,
-	                new Marca("Sony"), new Pantalla(6.1f, "OLED"),
+	                new Marca("Samsung"), new Pantalla(6.1f, "OLED"),
 	                new Dimension(157.0f, 68.0f, 8.2f), new Procesador("Snapdragon 888", 8, 2.84f)));
 
 	        moviles.add(new Movil("Mi 11", 256, 12, 196.0f, 108, 4600, true, 80, 799.99f,

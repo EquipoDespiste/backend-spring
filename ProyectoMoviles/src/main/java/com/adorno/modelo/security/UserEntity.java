62722e79e0178a0efba127b44208082d578576a3
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-02-28 02:03:18 +0100

    Add UserEntity, ERole and RoleEntity Class

--- ProyectoMoviles/src/main/java/com/adorno/modelo/security/UserEntity.java
+++ ProyectoMoviles/src/main/java/com/adorno/modelo/security/UserEntity.java
@@ -1,7 +1,56 @@
 package com.adorno.modelo.security;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
+
+@Data
+@NoArgsConstructor
+// Comentado hasta que lo demas vaya
+//@Entity
+//@Table(name = "users")
 public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Email(message = "no cumple formato")
+	@NotBlank(message = "no puede estar en blanco")
+	@Size(max = 80, message = "email demasiado largo")
+	private String email;
+	@NotBlank(message = "no puede estar en blanco")
+	@Size(max = 30, message = "demasiado largo el username")
+	@Column(unique = true)
+	private String username;
+	@NotBlank(message = "no puede estar en blanco")
+	// Este patron esta copiado de jose, NO QUITAR
+//	@Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d;,\\.]{8,}$", message = "Debe tener al menos 8 caracteres, contener al menos una mayúscula, una minúscula, un número y un símbolo entre coma, punto y coma, o punto")
+	private String password;
+
+
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+	private Set<RoleEntity> roles;
+	
+	public UserEntity(String email, String username, String password) {
+		super();
+		this.email = email;
+		this.username = username;
+		this.password = password;
+	}
 
 }

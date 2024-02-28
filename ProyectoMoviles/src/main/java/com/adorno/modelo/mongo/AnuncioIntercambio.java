d12d6bb3fd03d2495964e3c806db2f3feab1a50c
IgnacioOI <94613709+IgnacioOI@users.noreply.github.com>
2024-02-28 17:23:40 +0100

    Update AnuncioIntercambio.java
    AnuncioIntercambioCambio a anuncioIntercambio

--- ProyectoMoviles/src/main/java/com/adorno/modelo/mongo/AnuncioIntercambio.java
+++ ProyectoMoviles/src/main/java/com/adorno/modelo/mongo/AnuncioIntercambio.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AnuncioIntercambioCambio extends Anuncio{
+public class AnuncioIntercambio extends Anuncio{
 private Float vidaBateria;
 private List<PeticionIntercambio> peticiones;
 }

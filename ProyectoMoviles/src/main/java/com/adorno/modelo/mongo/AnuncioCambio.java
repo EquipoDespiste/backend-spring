104c66daf04487db0716863d9036ee418fd902b1
IgnacioOI <94613709+IgnacioOI@users.noreply.github.com>
2024-02-28 17:04:07 +0100

    issue91 crear anuncioCambio
    crear anuncio cambio con atribuuto vidaBateria

--- ProyectoMoviles/src/main/java/com/adorno/modelo/mongo/AnuncioCambio.java
+++ ProyectoMoviles/src/main/java/com/adorno/modelo/mongo/AnuncioCambio.java
@@ -1,12 +1,12 @@
 package com.adorno.modelo.mongo;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PeticionVenta extends Peticion{
-private Float precioPropuesto;
+public class AnuncioCambio extends Anuncio{
+private Float vidaBateria;
 }

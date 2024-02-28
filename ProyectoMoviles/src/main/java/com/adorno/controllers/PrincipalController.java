79ce687bfdfabf7aa0ef6858428a9daf9b8c8f01
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-02-29 00:56:05 +0100

    Issue#114 DONE: Add Spring Security Dependencies and PrincipalController

---
+++ ProyectoMoviles/src/main/java/com/adorno/controllers/PrincipalController.java
@@ -0,0 +1,52 @@
+package com.adorno.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adorno.modelo.dto.UserCreateDTO;
+import com.adorno.services.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("users")
+public class PrincipalController {
+	private final UserService userService;
+	
+	public PrincipalController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+
+	@GetMapping("hello")
+	public String hello() {
+		return "Hola que tal?";
+	}
+
+	@PreAuthorize("hasRole('ADMIN')")
+	@GetMapping("helloSecured")
+	public String helloS() {
+		return "SEGURO Hola que tal?";
+	}
+
+	@PostMapping("new")
+	public ResponseEntity<Boolean> createUser(@Valid @RequestBody UserCreateDTO userCreateDTO) {
+		if (userService.createUser(userCreateDTO)) {
+			return ResponseEntity.ok().body(true);
+		}
+		return ResponseEntity.badRequest().body(false);
+	}
+
+	@DeleteMapping("quita")
+	public ResponseEntity<Boolean> delete(@RequestParam String username){
+		return ResponseEntity.ok(userService.delete(username));
+	}
+}

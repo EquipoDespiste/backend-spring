4b03b85690720867f50f448307826ea6bd1f8407
IgnacioOI <94613709+IgnacioOI@users.noreply.github.com>
2024-02-28 17:21:10 +0100

    #issue96 crear PeticionIntercambio
    En la clase Anuncio cambio, se ha cambioado a AnuncioIntercambio, añadi la lista de peticion a AnuncioIntercambio y crear la peticion intercambio con un atributo del id de un anuncio interesado.

---
+++ ProyectoMoviles/src/main/java/com/adorno/modelo/mongo/PeticionIntercambio.java
@@ -0,0 +1,13 @@
+package com.adorno.modelo.mongo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PeticionIntercambio extends Peticion{
+private Long idAnuncioInteresado;
+
+}

b0d3b84d7d8c56e87a9d64bbda23789979609d0a
Inma <iperezs24m@gmail.com>
2024-02-29 19:25:49 +0100

    services a medias
    ahora sigo, problemas tecnicos

--- ProyectoMoviles/src/main/java/com/adorno/services/PeticionIntercambioService.java
+++ ProyectoMoviles/src/main/java/com/adorno/services/PeticionIntercambioService.java
@@ -3,22 +3,37 @@ package com.adorno.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
+import com.adorno.modelo.mapper.PeticionDTOMapper;
 import com.adorno.modelo.mongo.PeticionIntercambio;
 import com.adorno.modelo.sqlSecurity.UserEntity;
+import com.adorno.repositorio.IPeticionIntercambioRepository;
 @Service
 public class PeticionIntercambioService implements Services<PeticionIntercambio> {
 
+	private final IPeticionIntercambioRepository peticionIntercambioRepo;
+	private final PeticionDTOMapper peticionDTOmapper;
+	
+	
+	
+	public PeticionIntercambioService(IPeticionIntercambioRepository peticionIntercambioRepo,
+			PeticionDTOMapper peticionDTOmapper) {
+		super();
+		this.peticionIntercambioRepo = peticionIntercambioRepo;
+		this.peticionDTOmapper = peticionDTOmapper;
+	}
+
 	@Override
 	public boolean add(PeticionIntercambio t) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.peticionIntercambioRepo.save(t)!=null;
 	}
 
 	@Override
 	public boolean delete(long id) {
-		// TODO Auto-generated method stub
+	//	ObjectId idd=id;
+		Optional<PeticionIntercambio> peticionIntercambio= this.peticionIntercambioRepo.findById(id);
 		return false;
 	}
 

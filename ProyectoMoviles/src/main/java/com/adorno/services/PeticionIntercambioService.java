ffc09dd1854df57456c0d8d1c728ed1dcc695f58
rangeljuanma <jmrangelg04@iesarroyoharnina.es>
2024-02-29 01:08:00 +0100

    change Type IPeticion Service to Repository & fixing PeticionService

---
+++ ProyectoMoviles/src/main/java/com/adorno/services/PeticionIntercambioService.java
@@ -0,0 +1,79 @@
+package com.adorno.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.adorno.modelo.mongo.PeticionIntercambio;
+
+public class PeticionIntercambioService implements Services<PeticionIntercambio> {
+
+	@Override
+	public boolean add(PeticionIntercambio t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Optional<PeticionIntercambio> getById(long id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public List<PeticionIntercambio> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addAll(List<PeticionIntercambio> t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	
+
+//	@Override
+//	public boolean guardarSolicitud(Peticion p) {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//	@Override
+//	public List<Peticion> consultarPorTipo() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public List<Peticion> consultarPorFechas(LocalDate fechaInicial, LocalDate fechaFinal) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public List<Peticion> consultarEstado() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public List<Peticion> consultarPeticionesContestadas() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public List<Peticion> consultarPeticionesNoContestadas() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+}
